807adf9d888030b8a88c5d7bf90981ded629b40a
ajohnson1031 <aaron.johnson1031@gmail.com>
2019-10-31 20:48:41 -0400

    completed standard get calls

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // implement your API here
+const express = require("express");
+const db = require("./data/db.js");
+
+const server = express();
+
+server.listen(4001, () => {
+  console.log("=== Server Listening on Port 4000 ===");
+});
+
+server.use(express.json());
+
+//Standard API Call
+server.get("/", (req, res) => {
+  res.send("Hello, World.");
+});
+
+//Get All Users
+server.get("/users", (req, res) => {
+  db.find().then(users => {
+    res.status(200).json({ users: users });
+  });
+});
+
+//Get Specific User
+server.get("/user/:id", (req, res) => {
+  const { id } = req.params;
+  db.findById(id).then(user => {
+    res.status(200).json({ user: user });
+  });
+});
