172c16e626b996e371fba153f449745609bd9159
ajohnson1031 <aaron.johnson1031@gmail.com>
2019-11-01 20:15:11 -0400

    fixed all commands to mirror MVP requirements

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const db = require("./data/db.js");
 
 const server = express();
 
-server.listen(4001, () => {
+server.listen(4000, () => {
   console.log("=== Server Listening on Port 4000 ===");
 });
 
@@ -17,15 +17,90 @@ server.get("/", (req, res) => {
 
 //Get All Users
 server.get("/users", (req, res) => {
-  db.find().then(users => {
-    res.status(200).json({ users: users });
-  });
+  db.find()
+    .then(users => {
+      res.status(200).json({ users: users });
+    })
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "The user's information could not be retrieved." })
+    );
 });
 
 //Get Specific User
-server.get("/user/:id", (req, res) => {
+server.get("/users/:id", (req, res) => {
   const { id } = req.params;
-  db.findById(id).then(user => {
-    res.status(200).json({ user: user });
-  });
+  db.findById(id)
+    .then(user => {
+      user
+        ? res.status(200).json({ user: user })
+        : res.status(404).json({
+            message: "The user with the specified ID does not exist."
+          });
+    })
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "The user information could not be retrieved." })
+    );
+});
+
+//Add New User
+server.post("/users", (req, res) => {
+  const userInfo = req.body;
+
+  !userInfo.name || !userInfo.bio
+    ? res
+        .status(400)
+        .json({ errorMessage: "Please provide name and bio for the user." })
+    : db
+        .insert(userInfo)
+        .then(user => res.status(201).json({ success: true, user }))
+        .catch(err =>
+          res.status(500).json({
+            error: "There was an error while saving the user to the database"
+          })
+        );
+});
+
+//Update User
+server.put("/user/:id", (req, res) => {
+  const { id } = req.params;
+  const userInfo = req.body;
+
+  !userInfo.name || !userInfo.bio
+    ? res
+        .status(400)
+        .json({ errorMessage: "Please provide name and bio for the user." })
+    : db
+        .update(id, userInfo)
+        .then(user => {
+          if (user) res.status(200).json({ success: true, user });
+          else
+            res
+              .status(404)
+              .json({ message: `User with id ${id} does not exist...` });
+        })
+        .catch(err =>
+          res
+            .status(500)
+            .json({ error: "The user information could not be modified." })
+        );
+});
+
+//Delete User
+server.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  db.remove(id)
+    .then(deletedUser => {
+      if (deletedUser) res.status(204).end();
+      else
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+    })
+    .catch(err =>
+      res.status(500).json({ error: "The user could not be removed" })
+    );
 });
